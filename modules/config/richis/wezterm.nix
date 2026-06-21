9f22c20c769de7a9e286e2ad017c1e8aa2c0f787
Ricardo Ramirez <ricardo@richixs.dev>
2026-06-20 23:03:35 -0400

    add powerlevel 10k

--- modules/config/richis/wezterm.nix
+++ modules/config/richis/wezterm.nix
@@ -1,14 +1,21 @@
 { config, pkgs, ... }:
 
 {
+  fonts.fontconfig.enable = true;
+  home.packages = with pkgs; [
+    nerd-fonts.hack
+  ];
+
   programs.wezterm = {
     enable = true;
     extraConfig = ''
       local wezterm = require 'wezterm'
       local config = {}
-      
-      config.color_scheme = 'Retro'
-      config.font_size = 12.0
+
+      config.font = wezterm.font 'Hack Nerd Font'
+      config.font_size = 14.0
+
+      config.window_background_opacity = 0.85
       
       return config
     '';
--- modules/config/shared/zsh.nix
+++ modules/config/shared/zsh.nix
@@ -1,19 +1,35 @@
 { config, pkgs, ... }:
 
 {
+  home.file.".p10k.zsh".source = ./p10k.zsh;
+
   programs.zsh = {
     enable = true;
+
+    plugins = [
+      {
+        name = "powerlevel10k";
+        src = pkgs.zsh-powerlevel10k;
+        file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+      }
+    ];
+
     initContent = ''
+      POWERLEVEL9K_DISABLE_CONFIGURATION_WIZARD=true
+
       # fzf
       eval "$(fzf --zsh)"
+      
       # alias
       alias ls="lsd"
+
+      [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
     '';
 
     oh-my-zsh = {
       enable = true;
       plugins = [ "git" "docker" "sudo" "z" ];
-      theme = "robbyrussell";
+      theme = ""; 
     };
   };
 }
