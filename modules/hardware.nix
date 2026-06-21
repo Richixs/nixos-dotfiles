6e3dba454dd4547ca70f36a4f6c2ad79096eed36
Ricardo Ramirez <ricardo@richixs.dev>
2026-06-20 23:19:30 -0400

    enable 32 bits

--- modules/hardware.nix
+++ modules/hardware.nix
@@ -2,13 +2,17 @@
 
 {
   # NVIDIA
-  hardware.graphics.enable = true;
   services.xserver.videoDrivers = ["nvidia"];
   hardware.nvidia = {
     modesetting.enable = true;
     powerManagement.enable = true;
     open = false;
     nvidiaSettings = true;
+  };
+
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
   };
 
   # Print
